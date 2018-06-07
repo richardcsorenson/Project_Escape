f7e3837c917fced3cd4ae136424ab75d1456d925
Robidobi33 <robidobi33@gmail.com>
2018-06-06 19:21:57 -0600

    initial save

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,24 @@
 $(document).ready(function() {
 
+
     var npArr = ["Zion", "Arches", "Grand Canyon"];
 
+    var apiKey = "200285437-63e8df6ae924026feee1c05737ea2d62"
+    var queryURL = "";
+
+    function mapInfo(){
+
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response){
+
+            mapData = response.data;
+
+
+        });
+
+    }
     function newButton(){
     
         for(i=0;i<npArr.length;i++){
@@ -14,5 +31,13 @@ $(document).ready(function() {
         }
     }
 
+    $("#searchBtn").on("click", function(event){
+        event.preventDefault();
+
+        var value = ("#userInput").val().trim();
+
+    });
+
+   // $(documemt).on("click", "#npButton", mapInfo)
     newButton();
 });
