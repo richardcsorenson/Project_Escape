53f030de38c6622081b0b951f1653eab4a1b24c4
Robidobi33 <robidobi33@gmail.com>
2018-06-06 19:34:19 -0600

    test

---
+++ assets/javascript/logic.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    var npArr = ["Zion", "Arches", "Grand Canyon"];
+
+    var trailApiKey = "200285437-63e8df6ae924026feee1c05737ea2d62"
+
+    var QueryURL = "";
+
+    function mapInfo(){
+
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response){
+
+            mapData = response.data;
+
+
+        });
+
+    }
+    function newButton(){
+    
+        for(i=0;i<npArr.length;i++){
+
+            var newButton = $("<button>");
+            newButton.attr("id", "npButton");
+            newButton.text(npArr[i]);
+            $("#buttonBox").append(newButton);
+
+        }
+    }
+
+    $("#searchBtn").on("click", function(event){
+        event.preventDefault();
+
+        var value = ("#userInput").val().trim();
+
+    });
+
+   // $(documemt).on("click", "#npButton", mapInfo)
+    newButton();
+});
